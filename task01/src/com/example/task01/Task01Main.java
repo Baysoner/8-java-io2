b7da1e269d45fdeee709cdda81f6f52194a542a6
Artem Solovyov <artemsolovev121@gmail.com>
2025-03-13 20:43:28 +0500

    Соловьев Артём ПрИ-201. Задание 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,20 +1,38 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
         /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
         */
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        String ffprobe = "\"C:\\Users\\artem\\Downloads\\ffmpeg-2025-03-13-git-958c46800e-essentials_build\\bin\\ffprobe.exe\"";
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(ffprobe, "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath())
+                .redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+        String line;
+        String title;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            line = reader.readLine();
+            while (line != null) {
+                if (line.contains("title")) {
+                    title = line.split("=")[1].replace("\"", "");
+                    return title;
+                }
+                line = reader.readLine();
+            }
+        }
+
+        return null;
     }
 }
