a88102405ced2da4d29dfbadac9f87367c4292bf
Artem Solovyov <artemsolovev121@gmail.com>
2025-03-13 20:43:43 +0500

    Соловьев Артём ПрИ-201. Задание 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,8 +1,11 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -16,8 +19,13 @@ public class Task02Main {
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
+        List<Path> fileList = new ArrayList<>();
 
-        return null;
+        try (Stream<Path> paths = Files.walk(rootDir)) {
+            paths.filter(Files::isRegularFile)
+                    .forEach(fileList::add);
+        }
+
+        return fileList;
     }
 }
